862200d1394f223410ea3450cedb81c111a092f5
admin <admin@DESKTOP-8D38T2B>
2022-09-12 17:21:12 +0500

    Add func CountCategory, PrintData

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
+	"math/rand"
 	"net/http"
 	"strings"
+	"time"
 )
 
 type js struct {
@@ -18,11 +21,33 @@ type js struct {
 	Category    string `json:"Category"`
 }
 
+const times string = "2006-01-02"
+
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
 	var (
+		result = new([]js)
 		newstr []byte
 		search string
+		Year   int
+		Month  int
+		Day    int
 	)
+	fmt.Println("Введите дату 'От'\nВведите год:")
+	fmt.Scan(&Year)
+	fmt.Println("Введите месяц:")
+	fmt.Scan(&Month)
+	fmt.Println("Введите день:")
+	fmt.Scan(&Day)
+
+	if Month > 9 && Month < 13 {
+
+	}
+	date1 := fmt.Sprintf("%d-0%d-0%d", Year, Month, Day)
+	date2, _ := time.Parse(times, date1)
+	fmt.Println(date2)
+
 	resp, err := http.Get("https://api.publicapis.org/entries")
 	if err != nil {
 		fmt.Println("No response from request")
@@ -34,23 +59,81 @@ func main() {
 
 	newstr = body[strings.Index(string(body), "[") : strings.Index(string(body), "]")+1] //для массива нужно включать в строку "[" и "]"
 
-	//fmt.Println(string(newstr))
-
-	var result []js
-	if err := json.Unmarshal(newstr, &result); err != nil { // Parse []byte to the go struct pointer
+	if err := json.Unmarshal(newstr, result); err != nil { // Parse []byte to the go struct pointer
 		fmt.Println("Can not unmarshal JSON")
 	}
 
 	fmt.Println("Введите слово для поиска:")
 	fmt.Scan(&search)
 
-	fmt.Printf("%15s %100s", "Type", "Link")
-	for rec := range result {
-		switch searchn := strings.Contains(result[rec].Category, search); searchn {
-		case true:
-			fmt.Printf("%s %100s\n", result[rec].API, result[rec].Link)
-		case false:
-			break
+	err = PrintDataCategory(result, search)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	UniqueCategoryData(result)
+
+}
+
+func UniqueCategoryData(data *[]js) {
+	fmt.Println("UniqueCategoryData")
+	var (
+		uniquedata = make([]string, 0)
+		//count      int
+	)
+	fmt.Println("len:", len(uniquedata))
+Loopb:
+	for ind, val := range *data {
+		if ind == 0 {
+			uniquedata = append(uniquedata, val.Category)
 		}
+		for i := 0; i < len(uniquedata); i++ {
+			if val.Category == uniquedata[i] {
+				continue Loopb
+			}
+			uniquedata = append(uniquedata, val.Category)
+		}
+	}
+	for _, val := range uniquedata {
+		fmt.Println(val)
+	}
+}
+
+func CountCategoryData(data *[]js, strSort string) int {
+	var (
+		count int
+	)
+
+	for _, val := range *data {
+		if val.Category == strSort {
+			count++
+		}
+	}
+	return count
+}
+
+func PrintDataCategory(str *[]js, strSearch string) error {
+	var (
+		count       uint16 = uint16(len(*str))
+		count_range uint16
+	)
+
+	err := errors.New("Data is not found!")
+
+	if count != 0 {
+		fmt.Printf("%15s %100s\n", "Type", "Link")
+		for _, val := range *str {
+			switch searchstr := strings.Contains(val.Category, strSearch); searchstr {
+			case true:
+				fmt.Printf("%s %100s\n", val.API, val.Link)
+			case false:
+				count_range++
+				break
+			}
+		}
+
+		return nil
+	} else {
+		return err
 	}
 }
