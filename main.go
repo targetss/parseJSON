fb9f20da7358caf0e79f2d77f94910fda63c67cd
Targetss <bragin.q@gmail.com>
2022-10-06 00:17:35 +0500

    Edit method struct

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ import (
 	"math/rand"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
 	"time"
 )
@@ -87,21 +88,40 @@ func (p *JsonRaMInfo) ImportData(url string) {
 	defer respT.Body.Close()
 	body, err := io.ReadAll(respT.Body) // возвращает []byte
 
-	if err := json.Unmarshal(body, &rr); err != nil { // Parse []byte to the go struct pointer
+	if err := json.Unmarshal(body, &rr); err != nil {
 		fmt.Println(err)
 	}
 
-	*p = append(*p, rr) // записываем в массив структур данные первой страницы, отсюда вычисляем общее кол-во страниц
-
-	for _, val := range *p {
-		for _, val2 := range val.Results {
-			fmt.Println(val2.ID, val2.Image)
-			//go DownloadImage(val2.ID, val2.Image)
-			DownloadImage(val2.ID, val2.Image)
+	pathCfg := filepath.Join(pathConfig, fileNameCache) //Полный путь до файла
+	errchdir := os.Chdir(pathConfig)                    //изменяет текущий рабочий каталог на именованный каталог
+	if errchdir != nil {
+		err := os.Mkdir(pathConfig, 0755)
+		if err == nil {
+			log.Println("Создание директории для конфигурационных файлов")
+		}
+		_, errfle := os.Create(pathCfg)
+		if errfle != nil {
+			panic(fmt.Sprintf("Ошибка создания файла данный Json, ошибка: %v", err))
+		}
+	} else {
+		if _, err := os.Stat(pathCfg); os.IsNotExist(err) {
+			log.Println("Создание файла конфигурации")
+			os.Create(pathCfg)
 		}
 	}
 
-	//fmt.Println((*p)[0].Info.Pages) //сначала разыменовываем указатель, а после обращаемся по индексу
+	bytefile, _ := os.ReadFile(pathCfg)
+	if len(bytefile) != 0 {
+		var tempJson []JsonRaM
+		json.Unmarshal([]byte(bytefile), &tempJson)
+		if tempJson[0].Info.Count == rr.Info.Count {
+			fmt.Println("Считывание данных с файла конфигурации Json..")
+			*p = tempJson
+			return
+		}
+	}
+
+	*p = append(*p, rr) // записываем в массив структур данные первой страницы, отсюда вычисляем общее кол-во страниц
 
 	if (*p)[0].Info.Pages > 1 {
 		for i := 2; i <= (*p)[0].Info.Pages; i++ {
@@ -119,57 +139,80 @@ func (p *JsonRaMInfo) ImportData(url string) {
 				fmt.Println(err)
 			}
 			*p = append(*p, rr)
-
-			for _, val2 := range (*p)[i-1].Results {
-				//fmt.Println(val2.ID, val2.Image)
-				go DownloadImage(val2.ID, val2.Image)
-			}
 		}
 	}
+
+	filewrite, err := os.Create(pathCfg)
+	bt, _ := json.Marshal(*p)
+	countbt, errbt := filewrite.Write(bt)
+	if errbt != nil {
+		log.Println("Ошибка записи в файл json")
+	}
+	log.Println(fmt.Sprintf("Записано байт:%v\tБайт считано:%v", countbt, len(*p)))
+	filewrite.Close()
 }
 
-func (p *JsonRaMInfo) AddDataFile(f *io.Writer) error {
-	//дописать
-	return nil
+func (p *JsonRaMInfo) AddDataInFile() bool {
+	cacheDir, _ := os.UserCacheDir()
+	os.Mkdir(filepath.Join(cacheDir, "ConfigRaM"), 0755)
+	if _, fifeinfo := os.Stat(filepath.Join(cacheDir, "ConfigRaM", "JsonInfo.dat")); os.IsExist(fifeinfo) {
+		return false
+	}
+	fl, _ := os.Create(filepath.Join(cacheDir, "ConfigRaM", "JsonInfo.dat"))
+
+	//file, erropen := os.Open(filepath.Join(cacheDir, "ConfigRaM", "JsonInfo.dat"))
+	//if erropen != nil {
+	//	log.Println(erropen)
+	//}
+	//bt := make([]byte, len(*p))
+	bt, rtr := json.Marshal(*p)
+	fmt.Println(string(bt))
+	if rtr != nil {
+		log.Println("Ошибка Marshal json")
+	}
+	fl.Write(bt)
+	fl.Close()
+	return true
 }
 
 func (p *JsonRaMInfo) DownloadImageCharacter() {
 	cacheDir, _ := os.UserCacheDir()
-	os.Mkdir(fmt.Sprintf("%v/RaMImg", cacheDir), 0755)
+	fullpath := filepath.Join(cacheDir, "RaMImg") //join для того, чтобы корректно расставить разделители как в вашей ОС
+	os.Mkdir(fullpath, 0755)
 	for ind, _ := range *p {
 		for _, path2 := range (*p)[ind].Results {
-			/*if path2.ID%50 == 0 {
-				time.Sleep(500 * time.Millisecond)
-			}*/
 			go func(id int, url, path string) {
-				if _, errfile := os.Stat(fmt.Sprintf("%v/RaMImg/%v.jpeg", path, id)); !os.IsExist(errfile) {
-					log.Println("Файл существует")
+				if _, errfile := os.Stat(filepath.Join(fullpath, fmt.Sprintf("%v.jpeg", id))); os.IsExist(errfile) {
+					log.Printf("Файл с именем %v.jped существует\n", id)
 					return
 				}
 
 				//time.Sleep(100 * time.Millisecond) //срабатывает защита от ддос атак
-				fmt.Println("Gorutine go id:", path2.ID)
+				//fmt.Println("Gorutine go id:", id, "URL: ", url)
 				response, err := http.Get(url)
 				if err != nil {
 					log.Print(err)
 				}
 				if response.StatusCode == 429 {
+					//fmt.Println("Response code = 429, ID=", id, "\n", "Status=", response.Status, "\nStatusCode=", response.StatusCode)
+					response.Body.Close()
 					time.Sleep(8 * time.Second)
+
 					response, _ := http.Get(url)
-					fileCreate, _ := os.Create(fmt.Sprintf("%v/RaMImg/%v.jpeg", path, id))
+					fileCreate, _ := os.Create(filepath.Join(fullpath, fmt.Sprintf("%v.jpeg", id)))
 					io.Copy(fileCreate, response.Body)
 					fileCreate.Close()
 					response.Body.Close()
 					return
 				}
-				fileCreate, err := os.Create(fmt.Sprintf("%v/RaMImg/%v.jpeg", path, id))
+				fileCreate, err := os.Create(filepath.Join(fullpath, fmt.Sprintf("%v.jpeg", id)))
 				io.Copy(fileCreate, response.Body)
 
 				errEOF := errors.New("unexpected EOF")
 				loadImage, errdec := jpeg.Decode(fileCreate)
 				if !errors.Is(errdec, errEOF) {
 					resp, _ := http.Get(url)
-					fileCreate.Seek(0, io.SeekStart)
+					fileCreate.Seek(0, 0)
 					io.Copy(fileCreate, resp.Body)
 				}
 				_ = loadImage
@@ -181,8 +224,17 @@ func (p *JsonRaMInfo) DownloadImageCharacter() {
 	}
 }
 
-const times string = "2006-01-02"
-const url string = "https://rickandmortyapi.com/api/character/?page="
+const (
+	times string = "2006-01-02"
+	url   string = "https://rickandmortyapi.com/api/character/?page="
+)
+
+var (
+	userCachePath, _        = os.UserCacheDir()
+	nameDirCache     string = "RaM"
+	pathConfig              = filepath.Join(userCachePath, nameDirCache)
+	fileNameCache    string = "JsonData.dat"
+)
 
 type CardLayout struct {
 }
@@ -218,6 +270,7 @@ func main() {
 		cacheDir, _ = os.UserCacheDir()
 	)
 	result.ImportData(url)
+	//_ = result.AddDataInFile()
 	result.DownloadImageCharacter()
 
 	//RequestData(url, result)
@@ -257,7 +310,7 @@ func main() {
 	w.SetMainMenu(main_menu)
 
 	// =============== Поле карточки персонажа ================//
-	image := canvas.NewImageFromFile(fmt.Sprintf("%v/RaMImg/1.jpeg", cacheDir))
+	image := canvas.NewImageFromFile(fmt.Sprintf("%v\\RaMImg\\1.jpeg", cacheDir))
 	image.Resize(fyne.Size{Height: 250, Width: 250})
 
 	labelNameField := widget.NewLabel("Имя:")
@@ -297,7 +350,7 @@ func main() {
 	rr2 := container.NewVBox(rr, tableCard)
 
 	listID.OnSelected = func(id widget.ListItemID) {
-		img := canvas.NewImageFromFile(fmt.Sprintf("%v/RaMImg/%v.jpeg", cacheDir, id+1))
+		img := canvas.NewImageFromFile(fmt.Sprintf("%v\\RaMImg\\%v.jpeg", cacheDir, id+1))
 		img.FillMode = canvas.ImageFillOriginal
 		//img.Resize(fyne.Size{Height: 250, Width: 250})
 		//img.Move(fyne.NewPos(250, 250))
